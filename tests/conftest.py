28b57e5b907ccf864bd5e04bbddb61f115dd9b6d
Vadim <gureevvadim62@gmail.com>
2025-09-22 12:44:16 -0400

    add_f19

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,11 @@ from sklearn.preprocessing import StandardScaler
 
 @pytest.fixture(scope="session")
 def iris_dataset():
-    """Фикстура для датасета Iris"""
+
     iris = load_iris()
     return iris
 
 @pytest.fixture(scope="session")
 def scaler():
-    """Фикстура для scaler"""
+
     return StandardScaler()
